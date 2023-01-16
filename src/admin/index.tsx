79adad60f0b2faf2ebe8f549c174adae81ae8077
Noor Zainab <noor.zainab@maqsoodlabs.com>
2023-01-16 15:49:10 +0500

    pins list view and filter

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -2,6 +2,7 @@ import { Admin, Resource } from "react-admin";
 import dataProvider from "../dataProvider";
 import CategoriesList from "./components/categories/CategoriesList";
 import { CategoryCreate, CategoryEdit } from "./components/categories/CategoryForm";
+import PinsList from "./components/pins/PinsList";
 import { TagCreate, TagEdit } from "./components/tags/TagForm";
 import TagsList from "./components/tags/TagsList";
 import UserList from "./components/users/UserList";
@@ -12,6 +13,8 @@ const App = () => (
     <Resource name="users" list={UserList} />
     <Resource name="categories" list={CategoriesList} create={CategoryCreate} edit={CategoryEdit} />
     <Resource name="tags" list={TagsList} create={TagCreate} edit={TagEdit} />
+    <Resource name="pins" list={PinsList} />
+
   </Admin>
 );
 
