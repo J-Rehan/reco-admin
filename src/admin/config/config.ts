ff584c838af1cb3373698eeff3a7811022ee95e7
Noor Zainab <noor.zainab@maqsoodlabs.com>
2023-02-22 09:34:44 +0500

    prodd url

--- src/admin/config/config.ts
+++ src/admin/config/config.ts
@@ -1,19 +1,20 @@
 let apiUrl;
 let xApiKey;
-let firebaseConfig={};
+let firebaseConfig = {};
 
 if (process.env.REACT_APP_STATE === "production") {
-  apiUrl = "http://recoprodbackend-env.eba-imipffcp.us-east-1.elasticbeanstalk.com/v1";
+  apiUrl =
+    "http://recoprodbackend-env.eba-imipffcp.us-east-1.elasticbeanstalk.com/v1";
   firebaseConfig = {
-    apiKey: "AIzaSyBlWV3eNcFn_j0FPi9Pnn4A7Bvs-IA0dsU",
-    authDomain: "recoprod-d3f83.firebaseapp.com",
-    projectId: "recoprod-d3f83",
-    storageBucket: "recoprod-d3f83.appspot.com",
-    messagingSenderId: "509434945191",
-    appId: "1:509434945191:web:f84d238e36c048a0804582"
+    apiKey: "AIzaSyCu3zmwlwt6VpLj9hoIYumbxzYObqKJoGo",
+    authDomain: "reco-app-b6ae9.firebaseapp.com",
+    projectId: "reco-app-b6ae9",
+    storageBucket: "reco-app-b6ae9.appspot.com",
+    messagingSenderId: "31992326105",
+    appId: "1:31992326105:web:25d8b9ec9870f9412e1bc8",
+    measurementId: "G-ZG67QL6FGB",
   };
-  xApiKey= "c899a6108b2201ae57346b50270951ea";
-
+  xApiKey = "c899a6108b2201ae57346b50270951ea";
 } else if (process.env.REACT_APP_STATE === "staging") {
   apiUrl =
     "http://recobackend-env.eba-m6tynscj.us-east-1.elasticbeanstalk.com/v1";
@@ -24,9 +25,8 @@ if (process.env.REACT_APP_STATE === "production") {
     storageBucket: "recoapp-19f88.appspot.com",
     messagingSenderId: "680284497922",
     appId: "1:680284497922:web:4b74fc88d2cba01c155f1b",
-    
   };
-  xApiKey= "1ab2c3d4e5f61ab2c3d4e5f6";
+  xApiKey = "1ab2c3d4e5f61ab2c3d4e5f6";
 } else if (process.env.REACT_APP_STATE === "local") {
   apiUrl = "http://localhost:3000/v1";
   firebaseConfig = {
@@ -37,14 +37,13 @@ if (process.env.REACT_APP_STATE === "production") {
     messagingSenderId: "680284497922",
     appId: "1:680284497922:web:4b74fc88d2cba01c155f1b",
   };
-  xApiKey= "1ab2c3d4e5f61ab2c3d4e5f6";
-
+  xApiKey = "1ab2c3d4e5f61ab2c3d4e5f6";
 }
 
 const config = {
   apiUrl: apiUrl,
   firebaseConfig: firebaseConfig,
-  xApiKey:xApiKey
+  xApiKey: xApiKey,
 };
 
 export default config;
